6cb90a8ac772b132de1c808a75922780e893fd44
adieb10 <adieb1023@gmail.com>
2024-04-04 18:54:37 +0700

    i putu arya laksmana putra

---
+++ tugas5.py
@@ -0,0 +1,59 @@
+data_Biash_Qalbi_rabbani = {
+    "nama": "Biash Qalbi Rabbani",
+    "umur": 16,
+    "tanggal_lahir": "14 maret 2008",
+    "tempat_lahir": "sumbawa",
+    "kelas":"10 RPL 2"
+}
+
+print("===== Biash Qalbi Rabbani =====")
+for key, value in data_Biash_Qalbi_rabbani.items():
+    print(f"{key}: {value}")
+
+data_ulwatin = {
+    "nama": "ulwatin",
+    "umur": 17,
+    "tanggal_lahir": "19 oktober 2006",
+    "tempat_lahir": "lombok",
+    "kelas":"10 RPL 2"
+}
+
+print("===== Ulwatin =====")
+for key, value in data_ulwatin.items():
+    print(f"{key}: {value}")
+    
+data_Suci_nur_sabilah = {
+    "nama": "suci nur sabila",
+    "umur": 17,
+    "tanggal_lahir": "22 januari 2007",
+    "tempat_lahir": "mataram",
+    "kelas":"10 RPL 2"
+}
+
+print("===== suci nur sabilah =====")
+for key, value in data_Suci_nur_sabilah.items():
+    print(f"{key}: {value}")
+
+data_aldi_alfathani = {
+    "nama": "aldi alfathani",
+    "umur": 17,
+    "tanggal_lahir": "9 agustus 2007",
+    "tempat_lahir": "mataram",
+    "kelas":"10 RPL 2"
+}
+
+print("===== aldi alfathani =====")
+for key, value in data_aldi_alfathani.items():
+    print(f"{key}: {value}")
+    
+data_i_putu_arya_laksmana_putra = {
+    "nama": "i putu arya laksmana putra",
+    "umur": 17,
+    "tanggal_lahir": "12 agustus 2007",
+    "tempat_lahir": "mataram",
+    "kelas":"10 RPL 2"
+}
+
+print("===== i putu arya laksmana putra =====")
+for key, value in data_i_putu_arya_laksmana_putra.items():
+    print(f"{key}: {value}")
