945ecc9595a46a09c01d74f0f43c7c1c8148e8ed
tugas_1 <officialbiash269@gmail.com>
2024-04-04 14:34:09 +0700

    Biash Qalbi Rabbani

---
+++ tugas1.py
@@ -0,0 +1,11 @@
+data_Biash_Qalbi_rabbani = {
+    "nama": "Biash Qalbi Rabbani",
+    "umur": 16,
+    "tanggal_lahir": "14 maret 2008",
+    "tempat_lahir": "sumbawa",
+    "kelas":"10 RPL 2"
+}
+
+print("===== Biash Qalbi Rabbani =====")
+for key, value in data_Biash_Qalbi_rabbani.items():
+    print(f"{key}: {value}")
