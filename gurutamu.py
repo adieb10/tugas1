386754240bd024ff1a78102310c03a312d535cb7
adieb10 <adieb1023@gmail.com>
2024-04-04 18:30:07 +0700

    Ulwatin

---
+++ gurutamu.py
@@ -0,0 +1,23 @@
+data_Biash_Qalbi_rabbani = {
+    "nama": "Biash Qalbi Rabbani",
+    "umur": 16,
+    "tanggal_lahir": "14 maret 2008",
+    "tempat_lahir": "sumbawa",
+    "kelas":"10 RPL 2"
+}
+
+print("===== Biash Qalbi Rabbani =====")
+for key, value in data_Biash_Qalbi_rabbani.items():
+    print(f"{key}: {value}")
+
+data_ulwatin = {
+    "nama": "ulwatin",
+    "umur": 17,
+    "tanggal_lahir": "19 oktober 2006",
+    "tempat_lahir": "lombok",
+    "kelas":"10 RPL 2"
+}
+
+print("===== Ulwatin =====")
+for key, value in data_ulwatin.items():
+    print(f"{key}: {value}")
